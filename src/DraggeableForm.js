7825f3eee56d3f076f679e87b0b091dcef4726a1
paul3083 <paul30833083@gmail.com>
2023-09-15 09:55:32 +0900

    third commit

--- src/DraggeableForm.js
+++ src/DraggeableForm.js
@@ -4,66 +4,72 @@ import { BiTrash, BiGridVertical, BiPlus } from "react-icons/bi";
 import { v4 as uuidv4 } from "uuid";
 import styled from "styled-components";
 import Swal from "sweetalert2";
-import {Wheel} from "react-custom-roulette";
+import { Wheel } from "react-custom-roulette";
 import Logo from "./edcanlogo.svg";
-import EdcanChan from "./edcanchan.svg";
 
 const Roulette = () => {
     const [inputList, setInputList] = useState([
         {
-            id: uuidv4(),
-            text: "1등",
+            id: 1,
             desc: "G102 마우스",
+            prize: 1,
+            text: '1등'
         },
         {
-            id: uuidv4(),
-            text: "2등",
+            id: 2,
             desc: "소형 아크릴 스탠드",
+            prize: 1,
+            text: '2등'
         },
         {
-            id: uuidv4(),
-            text: "3등",
+            id: 3,
             desc: "키링",
+            prize: 1,
+            text: '3등'
         },
         {
-            id: uuidv4(),
-            text: "4등",
+            id: 4,
             desc: "안경닦이 or 마우스패드",
+            prize: 1,
+            text: '4등'
         },
         {
-            id: uuidv4(),
-            text: "5등",
-            desc: "약과"
+            id: 5,
+            desc: "약과",
+            prize: 1,
+            text: '5등'
         },
         {
-            id: uuidv4(),
-            text: "6등",
-            desc: "감자칩"
+            id: 6,
+            desc: "감자칩",
+            prize: 1,
+            text: '6등'
         },
     ]);
 
     // handle input change
     const [mustSpin, setMustSpin] = useState(false);
     const [prizeNumber, setPrizeNumber] = useState(0);
-    const [rouletteData, setRouletteData] = useState(inputList);
-    const [prize,setPrize] = useState([1,5,10,20,50,100]);
+    const [dataForWheel, setDataForWheel] = useState([
+        '1등', '2등', '3등', '4등', '5등', '6등'
+    ]);
+    const [prizes, setPrizes] = useState([1, 1, 1, 1, 1, 1]);
 
-    function customRandom(prize) {
+    useEffect(() => {
+        console.log(prizes);
+    }, [prizes]);
+
+    function customRandom(prizes) {
         // filter out indices with prize > 0
-        const validIndices = prize.reduce((acc, value, index) => {
+        const validIndices = prizes.reduce((acc, value, index) => {
             if (value > 0) {
                 acc.push(index);
             }
             return acc;
         }, []);
 
-        if (validIndices.length === 0) {
-            // All prizes are 0, return a default index (e.g., 0)
-            handleRemoveClick();
-            return 0;
-        }
-
-        const probabilities = validIndices.map(index => [0.005, 0.02, 0.05, 0.1, 0.2, 0.625][index]);
+        const probabilities = validIndices.map(index => [0, 0.02, 0.05, 0.1, 0.2, 0.63][index]);
+        console.log(probabilities)
         const randomNumber = Math.random();
         let cumulativeProbability = 0;
 
@@ -76,74 +82,22 @@ const Roulette = () => {
     }
 
     const handleSpinClick = () => {
-        const newPrizeNumber = customRandom(prize);
+        const newPrizeNumber = customRandom(prizes);
         setPrizeNumber(newPrizeNumber);
         setMustSpin(true);
 
         setTimeout(() => {
             Swal.fire({
                 title: "룰렛 결과",
-                text: `선택된 항목: ${rouletteData[newPrizeNumber].option}`,
+                text: `선택된 항목: ${dataForWheel[newPrizeNumber]}`,
                 icon: "success",
                 confirmButtonText: "확인",
             });
-            const updatedPrize = [...prize];
-            updatedPrize[newPrizeNumber] -= 1;
-            setPrize(updatedPrize);
+            const updatedPrizes = [...prizes];
+            updatedPrizes[newPrizeNumber] -= 1;
+            setPrizes(updatedPrizes);
         }, 2500);
     };
-
-    useEffect(() => {
-        // Check if prize[i] is 0 for each index i
-        const hasZeroPrize = prize.some(prizeValue => prizeValue === 0);
-
-        if (hasZeroPrize) {
-            handleRemoveClick(); // Call handleRemoveClick if prize[i] is 0
-        }
-    }, [prize]);
-
-
-    useEffect(() => {
-        const addShortString = inputList.map((item) => {
-            return {
-                completeOption: item.text,
-                option:
-                    item.text.length >= 30
-                        ? item.text.substring(0, 30).trimEnd() + "..."
-                        : item.text,
-            };
-        });
-        setRouletteData(addShortString);
-    }, [inputList]);
-    const handleInputChange = (e, index) => {
-        const { name, value } = e.target;
-        const list = [...inputList];
-        list[index][name] = value;
-        setInputList(list);
-    };
-
-    // handle click event of the Remove button
-    const handleRemoveClick = (index) => {
-        const rouletteResult = customRandom(prize);
-        const list = [...inputList];
-        list.splice(index, 1);
-        setInputList(list);
-    };
-
-    // handle click event of the Add button
-    const handleAddClick = () => {
-        setInputList([...inputList, { text: "", id: uuidv4() }]);
-    };
-
-    function handleOnDragEnd(result) {
-        if (!result.destination) return;
-
-        const items = Array.from(inputList);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
-
-        setInputList(items);
-    }
 
     return (
         <div className="main-form">
@@ -155,17 +109,17 @@ const Roulette = () => {
                     <div align="center" className="roulette-container">
                         <Wheel
                             mustStartSpinning={mustSpin}
-                            spinDuration={[0.2]}
+                            spinDuration={0.2}
                             prizeNumber={prizeNumber}
-                            data={rouletteData}
-                            outerBorderColor={["#D7F1FA"]}
-                            outerBorderWidth={[9]}
-                            innerBorderColor={["#D7F1FA"]}
-                            radiusLineColor={["tranparent"]}
-                            radiusLineWidth={[1]}
-                            textColors={["#D7F1FA"]}
+                            data={dataForWheel}
+                            outerBorderColor="#D7F1FA"
+                            outerBorderWidth={9}
+                            innerBorderColor="#D7F1FA"
+                            radiusLineColor="transparent"
+                            radiusLineWidth={1}
+                            textColors="#D7F1FA"
                             textDistance={55}
-                            fontSize={[20]}
+                            fontSize={20}
                             backgroundColors={[
                                 "#253746",
                                 "#425563",
@@ -183,85 +137,19 @@ const Roulette = () => {
                         </button>
                     </div>
                 </>
-                <Empty>
-                    <DragDropContext onDragEnd={handleOnDragEnd}>
-                        <Droppable droppableId="items">
-                            {(provided) => (
-                                <ul
-                                    className="items"
-                                    {...provided.droppableProps}
-                                    ref={provided.innerRef}
-                                    style={{ listStyle: "none" }}
-                                >
-                                    {inputList.map((x, index) => {
-                                        return (
-                                            <Draggable key={x.id} draggableId={x.id} index={index}>
-                                                {(provided) => (
-                                                    <li
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                        className="list-item"
-                                                    >
-                                                        <div className="item">
-                                                            <BiGridVertical />
-                                                            <input
-                                                                name="text"
-                                                                placeholder=""
-                                                                value={x.text}
-                                                                onChange={(e) => handleInputChange(e, index)}
-                                                                className="input"
-                                                            />
-                                                            <div className="btn-box">
-                                                                {inputList.length !== 1 && (
-                                                                    <button
-                                                                        className="button"
-                                                                        onClick={() => handleRemoveClick(index)}
-                                                                    >
-                                                                        <BiTrash />
-                                                                    </button>
-                                                                )}
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                )}
-                                            </Draggable>
-                                        );
-                                    })}
-                                    {provided.placeholder}
-                                </ul>
-                            )}
-                        </Droppable>
-                    </DragDropContext>
-                    <button
-                        onClick={handleAddClick}
-                        style={{ marginLeft: "2.1rem" }}
-                        className="button"
-                    >
-                        <BiPlus />
-                    </button>
-                </Empty>
             </Container>
             <Prize>
                 <PrizeBox>
-                    {
-                        inputList.map((item, index)=>(
-                        <PrizeContainer left={prize[index]}>
-                            {item.text}&nbsp;{item.desc}: 잔여 {prize[index]}개
+                    {inputList.map((item) => (
+                        <PrizeContainer key={item.id} left={prizes[item.id - 1]}>
+                            {`${item.text} ${item.desc}: 잔여 ${prizes[item.id - 1]}`}
                         </PrizeContainer>
-                    ))
-                    }
+                    ))}
                 </PrizeBox>
             </Prize>
         </div>
     );
 };
-
-const Chan = styled.image`
-  display: flex;
-  width: 100px;
-  height: 100px;
-`;
 
 const PrizeContainer = styled.div`
   width: 500px;
